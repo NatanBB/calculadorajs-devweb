d2f801d3a66370dcc83d1f4bf87fbcea7c813b92
NatanBB <natanborba77@gmail.com>
2022-04-06 11:00:22 -0300

    v2

--- js/Bhaskara/bhaskara.js
+++ js/Bhaskara/bhaskara.js
@@ -1,31 +1,21 @@
 function Calcular(){
   
-  var valorA = document.querySelector("#a");
-  var valorB = document.querySelector("#b");
-  var valorC = document.querySelector("#c");
-  
-  var a = valorA.value;
-  var b = valorB.value;
-  var c = valorC.value;
-
-  console.log(a);
-  console.log(b);
-  console.log(c);
+  var a = document.getElementById("a").value;
+  var b = document.getElementById("b").value;
+  var c = document.getElementById("c").value;
 
   var positivo;
   var negativo;
 
   var delta = (b * b) - 4 * (a * c);
 
-  document.getElementById("deltaValue").innerHTML=(delta);
 
-  if (delta < 0 ) {
-    document.getElementById("textDelta").innerHTML=("A equação não possui raízes")
+if (delta < 0 ) {
+    document.getElementById("text").innerHTML=("<p>A equação não possui raízes</p>")
   } else {
     positivo = (-b + Math.sqrt(delta)) / (2 * a);
     negativo = (-b - Math.sqrt(delta)) / (2 * a);
 
-    document.getElementById("x1Value").innerHTML=("" + positivo + "</br>");
-    document.getElementById("x2Value").innerHTML=("" + negativo);
+    document.getElementById("text").innerHTML=(`<p>x1: ${positivo} x2: ${negativo} </p>`);
   }
 }
