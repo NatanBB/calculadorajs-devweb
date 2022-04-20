fb85c39f590575bbb0be32b5cbc82f3bc21526a1
NatanBB <natanborba77@gmail.com>
2022-04-20 10:08:59 -0300

    Adicionando testes

--- js/Bhaskara/bhaskara.js
+++ js/Bhaskara/bhaskara.js
@@ -1,4 +1,4 @@
-function Calcular(){
+function Calcular(){ // função para ser usada no HTML
   
   var a = document.getElementById("a").value;
   var b = document.getElementById("b").value;
@@ -10,12 +10,33 @@ function Calcular(){
   var delta = (b * b) - 4 * (a * c);
 
 
-if (delta < 0 ) {
+  if (delta < 0 ) {
     document.getElementById("text").innerHTML=("<p>A equação não possui raízes</p>")
   } else {
     positivo = (-b + Math.sqrt(delta)) / (2 * a);
     negativo = (-b - Math.sqrt(delta)) / (2 * a);
 
-    document.getElementById("text").innerHTML=(`<p>x1: ${positivo} x2: ${negativo} </p>`);
+    document.getElementById("text").innerHTML=(`<p>x1: ${positivo} x2: ${negativo} delta: ${delta} </p>`);
   }
 }
+
+function bhaskaraTest(a, b, c){ //Função para testes
+
+  var positivo;
+  var negativo;
+
+  var delta = (b * b) - 4 * (a * c);
+
+
+  if (delta < 0 ) {
+    return;
+  } else {
+    positivo = (-b + Math.sqrt(delta)) / (2 * a);
+    negativo = (-b - Math.sqrt(delta)) / (2 * a);
+
+  }
+
+  return positivo, negativo, delta;
+}
+
+module.exports = bhaskaraTest;
---
+++ js/Bhaskara/bhaskara.test.js
@@ -0,0 +1,9 @@
+const bhaskaraTest = require('./bhaskara');
+
+it('Calcula bhaskara', () => {
+  expect(bhaskaraTest(2,12,-14)).toBe(256,1,-7)
+})
+
+it('Calcula bhaskara', () => {
+  expect(bhaskaraTest(2,12,-14)).toBe(256,1,-7)
+})
